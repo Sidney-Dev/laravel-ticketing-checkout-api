c8e663c7e5a8edb216528eb9f8a931ff977048df
SidneySousa <sidney.sousa@mtn.com>
2025-12-18 13:13:56 +0200

    feat: checkout controller with status code

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    
+    public function checkout(Request $request)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => 'Response processed',
+        ], 400);
+    }
+}
