c0edd02d0fca90aa59c578dce196707dcd46ecd6
SidneySousa <sidney.sousa@mtn.com>
2025-12-24 00:32:22 +0200

    feat: added db transaction facades

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -5,37 +5,40 @@ namespace app\Services;
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Ticket;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutService
 {
     public function checkout(array $orderData): Order
     {
-        $user = User::latest()->first();
+        return DB::transaction(function () use ($orderData) {
+            $user = User::latest()->first();
 
-        $order = $user->orders()->create([
-            'status' => 'pending',
-            'total' => 0,
-        ]);
-
-        $total = 0;
-
-        foreach ($orderData['tickets'] as $item) {
-            $ticket = Ticket::findOrFail($item['ticket_id']);
-
-            // update inventory
-            $ticket->decrement('available_quantity', $item['quantity']);
-            $lineTotal = $ticket->price * $item['quantity'];
-
-            $order->items()->create([
-                'ticket_id' => $ticket->id,
-                'quantity' => $item['quantity'],
-                'price' => $ticket->price,
+            $order = $user->orders()->create([
+                'status' => 'pending',
+                'total' => 0,
             ]);
 
-            $total += $lineTotal;
-        }
-        $order->update(['total' => $total]);
+            $total = 0;
 
-        return $order;
+            foreach ($orderData['tickets'] as $item) {
+                $ticket = Ticket::findOrFail($item['ticket_id']);
+
+                // update inventory
+                $ticket->decrement('available_quantity', $item['quantity']);
+                $lineTotal = $ticket->price * $item['quantity'];
+
+                $order->items()->create([
+                    'ticket_id' => $ticket->id,
+                    'quantity' => $item['quantity'],
+                    'price' => $ticket->price,
+                ]);
+
+                $total += $lineTotal;
+            }
+            $order->update(['total' => $total]);
+
+            return $order;
+        });
     }
 }
